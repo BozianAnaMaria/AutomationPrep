2d9530e28c3ebb2d95cc265f59e8c0a0897cf0bd
Ana-Maria Bozian <AMbozian6@gmail.com>
2025-05-14 15:08:43 +0300

    Implemented Driver Manager

--- src/main/java/org/example/TestRunner.java
+++ src/main/java/org/example/TestRunner.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import org.example.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
 public class TestRunner {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = DriverManager.getInstance().getDriver();
+        driver.get("https://www.google.com");
+
+        String FirstTabName = driver.getWindowHandle();
+
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://moodle.usm.md/");
+        Thread.sleep(2000);
+        driver.close();
+
+        driver.switchTo().window(FirstTabName);
+        driver.get("https://github.com/");
+        Thread.sleep(2000);
+        driver.quit();
+
     }
 }
